9496e807208457e4d305d2a6329a70495cf6ee66
Eddie Atkinson <>
2021-04-25 21:51:13 +0800

    adds backend for mailing list add and delete

--- src/services/users/hooks/mailChimpSync.js
+++ src/services/users/hooks/mailChimpSync.js
@@ -1,23 +1,42 @@
 const { checkContext } = require('feathers-hooks-common');
-// const _ = require('lodash');
+const client = require('mailchimp-marketing');
+const { InternalServerError } = require('@feathersjs/errors');
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (options = {}) => {
+
+
+const updateMailingList = async (client, mailListId, email, status) => {
+  return client.lists.batchListMembers(mailListId, {
+    members: [{
+      email_address: email,
+      status
+    }],
+  });
+};
+
+module.exports = (app) => {
   return async context => {
     checkContext(context, 'after', ['create', 'update', 'patch']);
+    const mailchimpApiKey = app.get('mailchimpApiKey');
+    const mailListId = app.get('mailchimpMailListId');
 
     const { result, params } = context;
+    client.setConfig({
+      apiKey: mailchimpApiKey,
+      server: 'access 3',
+    });
 
     const { existing } = params;
     const isJoining = !existing.preferences.joinedAt && result.joinedAt;
     const hasLeft = existing.preferences.joinedAt && !result.joinedAt;
     console.log(result);
-    if (isJoining) {
-      // add to result.email to mail list
-
-    } else if (hasLeft) {
-      // remove result.email from mailing list
+    if (isJoining || hasLeft) {
+      const status = isJoining ? 'subscribed' : 'unsubscribed';
+      const response = await updateMailingList(client, mailListId, result.email, status);
+      if (response.errors.length) {
+        return new InternalServerError(response.errors);
+      }
     }
+    console.log('done');
     return context;
   };
 };
