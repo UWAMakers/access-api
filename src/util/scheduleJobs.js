71346c1822f0f2e611b06e7441d0e25ae2fd2440
Eddie Atkinson <>
2021-06-12 19:55:20 +0800

    fix bad merge

--- src/util/scheduleJobs.js
+++ src/util/scheduleJobs.js
@@ -5,15 +5,11 @@ module.exports = (app) => {
   let lastCompletionSync = 0;
   setInterval(async () => {
     // check the quizzes every 5 min
-<<<<<<< HEAD
-    await processQuizzes(app);
-=======
     try {
       await processQuizzes(app);
     } catch (err) {
       console.error(err);
     }
->>>>>>> origin
 
     // refresh the completions (checks for expiries) every day shortly after 7am.
     if ((new Date()).getHours() === 7 && lastCompletionSync < Date.now() - (120 * 60 * 1000)) {
